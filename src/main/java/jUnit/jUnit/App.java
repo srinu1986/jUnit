9ee5c0b209ccac20bedf1ac0cbdf62575acac1bc
unknown <srinu.velam@gmail.com>
2018-03-31 23:43:20 +0530

    commit message

---
+++ src/main/java/jUnit/jUnit/App.java
@@ -0,0 +1,13 @@
+package jUnit.jUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
